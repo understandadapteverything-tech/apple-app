5e7103f9037dc5df630a0eaca9d70c0e01cb5b11
Adapt Incessant <adaptincessant@MacBook-Air-khxng-Adapt.local>
2025-11-06 01:54:17 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import NavBar from "./components/NavBar.jsx"
+
+const App = () => {
+  return (
+    <mian>
+        <NavBar />
+        <h1> Hello Macbook</h1>
+    </mian>
+  )
+}
+
+export default App
